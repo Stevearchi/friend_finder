128cb646e5e973a319799b9c71df7d51e372837b
Steve Archibald <stevearchi21@gmail.com>
2019-07-10 20:56:31 -0600

    Finished except for modal

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # friend_finder
-A "dating" type app
+A "dating" type app.  User fills out survey form.  This data is saved to an array.  Then that data is checked against all of the other people in the array.  The best match is selected and shown to the user.
+
+
+Technologies used:
+* JQuery
+* Node.js
+* Express
+* Bootstrap/BootSwatch
+
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,8 +1,8 @@
 var friends = require('../data/friends');
 
 
-  // API GET Requests
-  // Below code handles when users "visit" a page.
+// API GET Requests
+// Below code handles when users "visit" a page.
 
 module.exports = function (app) {
 
@@ -11,7 +11,29 @@ module.exports = function (app) {
     });
 
     app.post('/api/friendslist', function (req, res) {
-        friends.push(req.body);
+
+        var newProfile = req.body;
+        for (var i = 0; i < newProfile.scores.length; i++) {
+            newProfile.scores[i] = parseInt(newProfile.scores[i]);
+        };
+
+        // Math
+        var bestMatchScore = 41; // the farthest apart two people's total scores can be is 40
+        var bestMatch = {};
+        for (var i = 0; i < friends.length; i++) {
+            var score = 0;
+            for (var j = 0; j < 9; j++) {
+                var miniScore = Math.abs(friends[i].scores[j] - newProfile.scores[j]);
+                score += miniScore;
+            }
+            if (score < bestMatchScore) {
+                bestMatchScore = score;
+                bestMatch = friends[i];
+            }
+        }
+     
+        
+        friends.push(newProfile);
         res.end();
     });
 };
